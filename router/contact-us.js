22a0425ff14669b3f85a12ae3a44cc034e3d2716
beltagy97 <43505298+beltagy97@users.noreply.github.com>
2019-01-30 16:03:38 +0200

    newsfeed added but still needs styling + fixed container issues

--- router/contact-us.js
+++ router/contact-us.js
@@ -1,9 +1,16 @@
 var express = require("express");
 var router  = express.Router();
+var bodyParser = require("body-parser");
 
 
 router.get("/contact-us", function(req, res) {
     res.render("contact-us");
   });
+
+router.post("/contact-us" , function(req,res){
+      console.log("You hit Post request");
+      console.log(req.body);
+      res.render("contact-us");
+});
 
   module.exports = router;
