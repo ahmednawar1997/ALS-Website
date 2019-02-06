cec226bbc45cab1e9c234877a9749b929a5d8efc
beltagy97 <43505298+beltagy97@users.noreply.github.com>
2019-02-06 20:19:18 +0200

    add new design for admission page

--- js/admissionProcess.js
+++ js/admissionProcess.js
@@ -1,33 +1,48 @@
 $(document).ready(function(){
     $("#requirementsButton").click(function(){
-        $('#list-tab a').removeClass('active');
+        $('#list-tab button').removeClass('active');
+        $('#list-tab button').removeClass('btn-secondary');
+        $('#list-tab button').addClass('btn-danger')
         $("#requirementsButton").addClass('active');
+        $("#requirementsButton").removeClass('btn-danger');
+        $("#requirementsButton").addClass('btn-secondary');
           $('#requirementsTab').css("display", "block");
           $('#applyNowTab').css("display", "none");
           $('#admissionProcessTab').css("display", "none");
           $('#pagingBar').text("Requirements");
+          $('#title').text("Requirements");
     });
   });
   
   $(document).ready(function(){
     $("#applyNowButton").click(function(){
-        $('#list-tab a').removeClass('active');
+        $('#list-tab button').removeClass('active');
+        $('#list-tab button').removeClass('btn-secondary');
+        $('#list-tab button').addClass('btn-danger');
         $("#applyNowButton").addClass('active');
+        $("#applyNowButton").removeClass('btn-danger');
+        $("#applyNowButton").addClass('btn-secondary');
         $('#requirementsTab').css("display", "none");
         $('#applyNowTab').css("display", "block");
         $('#admissionProcessTab').css("display", "none");
         $('#pagingBar').text("Apply Now");
+        $('#title').text("Apply Now");
     });
   });
   
   $(document).ready(function(){
     $("#admissionButton").click(function(){
-        $('#list-tab a').removeClass('active');
+        $('#list-tab button').removeClass('active');
+        $('#list-tab button').removeClass('btn-secondary');
+        $('#list-tab button').addClass('btn-danger');
         $("#admissionButton").addClass('active');
+        $("#admissionButton").removeClass('btn-danger');
+        $("#admissionButton").addClass('btn-secondary');
         $('#requirementsTab').css("display", "none");
         $('#applyNowTab').css("display", "none");
         $('#admissionProcessTab').css("display", "block");
         $('#pagingBar').text("Admission Process");
+        $('#title').text("Admission Process");
    
     });
   });
