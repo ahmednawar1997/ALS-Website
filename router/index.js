8b4b3f88b16617a8823fb8d2528e25747b71826c
Ahmed Nawar <ahmednawar2007@gmail.com>
2019-02-07 19:15:59 +0200

    Add jobs page and viewJob Add partials

--- router/index.js
+++ router/index.js
@@ -1,10 +1,27 @@
 var express = require("express");
-var router  = express.Router();
+var router = express.Router();
+var con = require("../db/connection");
 
 
-router.get("/", function(req, res) {
-    res.render("index");
+router.get("/", function (req, res) {
+  res.render("index");
+});
+
+
+router.get("/jobs", function (req, res) {
+  res.render("jobs");
+});
+
+router.get("/jobs/:id", function (req, res) {
+  console.log(req.params.id);
+  //go fetch the job with this id and render the job page
+  let sql = "SELECT * FROM Jobs where ID = ?"
+  con.query(sql, [req.params.id], function (err, jobs) {
+    if (err) console.log(err);
+    res.render("viewJob",{ jobs: jobs });
   });
 
+});
 
-  module.exports = router;
+
+module.exports = router;
