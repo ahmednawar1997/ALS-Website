86f782f145825d2c1672d57c42c527ab704042f0
beltagy97 <43505298+beltagy97@users.noreply.github.com>
2019-02-07 20:56:23 +0200

    add contact-us form to insert to database

--- router/contact-us.js
+++ router/contact-us.js
@@ -1,16 +1,20 @@
 var express = require("express");
 var router  = express.Router();
 var bodyParser = require("body-parser");
-
+var con = require("../db/connection");
 
 router.get("/contact-us", function(req, res) {
     res.render("contact-us");
   });
 
 router.post("/contact-us" , function(req,res){
-      console.log("You hit Post request");
-      console.log(req.body);
-      res.render("contact-us");
+    var query = "INSERT INTO ContactUs ( ParentName, ChildName, Nationality, PhoneNumber, Email, Message) VALUES (?,?,?,?,?,?)";
+
+    con.query(query, [req.body.name , req.body.cname , req.body.nationality , req.body.phone , req.body.email , req.body.message] , function (err, data) {
+      if (err) console.log(err);
+      console.log(data);
+  });
+  res.render("contact-us");
 });
 
   module.exports = router;
