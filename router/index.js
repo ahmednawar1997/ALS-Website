bbc27e84ab37d854f8759833961f8da9e47c8155
Ahmed Nawar <ahmednawar2007@gmail.com>
2019-02-08 14:49:06 +0200

    add applicants form insert applicants to DB No upload for the cv yet

--- router/index.js
+++ router/index.js
@@ -9,18 +9,34 @@ router.get("/", function (req, res) {
 
 
 router.get("/jobs", function (req, res) {
-  res.render("jobs");
+  let sql = "SELECT * FROM Jobs"
+  con.query(sql, function (err, jobs) {
+    if (err) console.log(err);
+    res.render("jobs", { jobs: jobs });
+  });
 });
 
 router.get("/jobs/:id", function (req, res) {
-  console.log(req.params.id);
   //go fetch the job with this id and render the job page
   let sql = "SELECT * FROM Jobs where ID = ?"
   con.query(sql, [req.params.id], function (err, jobs) {
     if (err) console.log(err);
-    res.render("viewJob",{ jobs: jobs });
+    res.render("viewJob", {job: jobs[0]});
   });
+});
 
+router.post("/jobs/apply/:jobId", function (req, res) {
+  console.log(req.params.jobId);
+  console.log(req.body.name);
+  console.log(req.body.email);
+  console.log(req.body.phone);
+  //go fetch the job with this id and render the job page
+
+  let sql = "  INSERT INTO Applicants (Name, Email, Phone, JobID) VALUES (?, ?, ?, ?)"
+  con.query(sql, [req.body.name, req.body.email, req.body.phone, req.params.jobId], function (err, insertedApplicant) {
+    if (err) console.log(err);
+    res.redirect('/jobs');
+  });
 });
 
 
