ba88ead68fac344b095a1aa6d4a188530cd9271e
beltagy97 <43505298+beltagy97@users.noreply.github.com>
2019-02-14 17:42:15 +0200

    add database to admission form

--- router/admission.js
+++ router/admission.js
@@ -9,7 +9,7 @@ var date = (new Date()).getFullYear();
 
 var storage =   multer.diskStorage({
   destination: function (req, file, callback) {
-    callback(null, './upload');
+    callback(null, './upload/admissions');
   },
   filename: function (req, file, callback) {
     callback(null, date +'-' + file.originalname );
@@ -29,10 +29,21 @@ router.get("/admission-process", function(req, res) {
   
 router.post("/admission-process", upload.any() ,  function(req , res)
 { 
-    console.log(req.files);
+    
 
-    // still DB to be done
+    var query = "INSERT INTO Admission (ChildName, DateOfBirth, Age, NumberOfSiblings, Gender, GuardianFullName, Occupation, Nationality, "+
+      "Address, Profession, UniversityDegree, PhoneNumber, Email, FatherID, UniversityCertificate, GrandFatherName, GrandFatherProfession, GrandFatherAddress)" + 
+      "VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)";
+    
+      var data = [req.body.childname , req.body.dob , req.body.age ,req.body.siblings , req.body.gridRadios , req.body.parent , req.body.occupation , req.body.nationality , req.body.address , req.body.profession , req.body.university , req.body.phone , req.body.email , req.files[0].path , req.files[1].path , req.body.grandName , req.body.grandProfession , req.body.grandAdrress ];
+    
+    con.query(query , data ,  function(err , data)
+    {
+      if(err) console.log(err);
+      console.log(data);
+    });
 
+    
     res.render("admission-process" , {date : date});
 
 });
