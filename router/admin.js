fa8f8c765191da36cacad3fe425f6d086a9c4d4d
OmarKatary <OmarKatary@users.noreply.github.com>
2019-02-20 22:15:48 +0200

    Added news submission backend

--- router/admin.js
+++ router/admin.js
@@ -3,7 +3,7 @@ var router = express.Router();
 var con = require("../db/connection");
 
 router.get("/admin", (req, res) => {
-    var query = "SELECT * FROM applicants, jobs " +
+    var query = "SELECT * FROM Applicants, jobs " +
         "WHERE applicants.JobID = jobs.ID";
     con.query(query, [], (err, jobInfo) => {
         if (err) console.log("ERROR", err);
@@ -12,16 +12,17 @@ router.get("/admin", (req, res) => {
 });
 
 router.post("/admin/new-article", (req, res) => {
-    var query = "INSERT INTO news (Title, Text, Date) " +
+    var query = "INSERT INTO News (Title, Text, Date) " +
         "VALUES (?,?,?)";
-    var data = [req.body.articleTitle, req.body.text, new Date()];
+    var data = [req.body.articleTitle, req.body.articleText, new Date()];
     con.query(query, data, (err) => {
         if (err) console.log("ERROR", err);
     });
+    res.render("admin");
 });
 
 router.post("/admin/new-job", (req, res) => {
-    var query = "INSERT INTO jobs (JobTitle, Description, JobPostDate, EndDate) " +
+    var query = "INSERT INTO Jobs (JobTitle, Description, JobPostDate, EndDate) " +
         "VALUES (?,?,?,?)";
     var data = [req.body.jobTitle, req.body.jobDescription, req.body.jobStartDate, req.body.jobEndDate];
     con.query(query, data, (err) => {
