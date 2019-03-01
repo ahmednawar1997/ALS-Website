bfec4ffbe0dfc94c2f379d4df16f4c3613249c47
beltagy97 <43505298+beltagy97@users.noreply.github.com>
2019-03-01 21:17:34 +0200

    add complaints

--- router/index.js
+++ router/index.js
@@ -3,21 +3,21 @@ var router = express.Router();
 var con = require("../db/connection");
 var upload = require("../helpers/multerConfig");
 
-router.get("/", function(req, res) {
+router.get("/", function (req, res) {
   res.render("index");
 });
 
-router.get("/activity", function(req, res) {
+router.get("/activity", function (req, res) {
   res.render("activityList");
 });
 
-router.get("/activity/:id", function(req, res) {
+router.get("/activity/:id", function (req, res) {
   res.render("activityView");
 });
 
-router.get("/jobs", function(req, res) {
+router.get("/jobs", function (req, res) {
   let sql = "SELECT * FROM Jobs";
-  con.query(sql, function(err, jobs) {
+  con.query(sql, function (err, jobs) {
     if (err) console.log(err);
     res.render("jobs", {
       jobs: jobs
@@ -25,9 +25,9 @@ router.get("/jobs", function(req, res) {
   });
 });
 
-router.get("/jobs/:id", function(req, res) {
+router.get("/jobs/:id", function (req, res) {
   let sql = "SELECT * FROM Jobs where ID = ?";
-  con.query(sql, [req.params.id], function(err, jobs) {
+  con.query(sql, [req.params.id], function (err, jobs) {
     if (err) console.log(err);
     res.render("viewJob", {
       job: jobs[0]
@@ -35,7 +35,7 @@ router.get("/jobs/:id", function(req, res) {
   });
 });
 
-router.post("/jobs/apply/:jobId", upload.any(), function(req, res) {
+router.post("/jobs/apply/:jobId", upload.any(), function (req, res) {
   let sql =
     "  INSERT INTO Applicants (Name, Email, Phone, CV, JobID) VALUES (?, ?, ?, ?, ?)";
   con.query(
@@ -47,20 +47,20 @@ router.post("/jobs/apply/:jobId", upload.any(), function(req, res) {
       req.files[0].path,
       req.params.jobId
     ],
-    function(err, insertedApplicant) {
+    function (err, insertedApplicant) {
       if (err) console.log(err);
       res.redirect("/jobs");
     }
   );
 });
 
-router.get("/overview", function(req, res) {
+router.get("/overview", function (req, res) {
   res.render("overview");
 });
 
-router.get("/news", function(req, res) {
+router.get("/news", function (req, res) {
   let sql = "SELECT * FROM News";
-  con.query(sql, function(err, news) {
+  con.query(sql, function (err, news) {
     if (err) console.log(err);
     res.render("news", {
       news: news
@@ -68,14 +68,18 @@ router.get("/news", function(req, res) {
   });
 });
 
-router.get("/news/:id", function(req, res) {
+router.get("/news/:id", function (req, res) {
   let sql = "SELECT * FROM News where ID = ?";
-  con.query(sql, [req.params.id], function(err, news) {
+  con.query(sql, [req.params.id], function (err, news) {
     if (err) console.log(err);
     res.render("viewNews", {
       news: news[0]
     });
   });
+});
+
+router.get("/complaints", function (req, res) {
+  res.render("complaints");
 });
 
 module.exports = router;
