49d4211269ddcef25e0cff41d9f113bfc5ea456e
Amr Ramadan <amr_ramadan97@outlook.com>
2019-03-02 00:08:06 +0200

    add routes for new pages

--- router/index.js
+++ router/index.js
@@ -3,21 +3,30 @@ var router = express.Router();
 var con = require("../db/connection");
 var upload = require("../helpers/multerConfig");
 
-router.get("/", function (req, res) {
+router.get("/", function(req, res) {
   res.render("index");
 });
 
-router.get("/activity", function (req, res) {
+router.get("/activity", function(req, res) {
   res.render("activityList");
 });
+router.get("/drive", function(req, res) {
+  res.render("drive");
+});
+router.get("/survey", function(req, res) {
+  res.render("survey");
+});
+router.post("/survey", function(req, res) {
+  res.render("survey");
+});
 
-router.get("/activity/:id", function (req, res) {
+router.get("/activity/:id", function(req, res) {
   res.render("activityView");
 });
 
-router.get("/jobs", function (req, res) {
+router.get("/jobs", function(req, res) {
   let sql = "SELECT * FROM Jobs";
-  con.query(sql, function (err, jobs) {
+  con.query(sql, function(err, jobs) {
     if (err) console.log(err);
     res.render("jobs", {
       jobs: jobs
@@ -25,9 +34,9 @@ router.get("/jobs", function (req, res) {
   });
 });
 
-router.get("/jobs/:id", function (req, res) {
+router.get("/jobs/:id", function(req, res) {
   let sql = "SELECT * FROM Jobs where ID = ?";
-  con.query(sql, [req.params.id], function (err, jobs) {
+  con.query(sql, [req.params.id], function(err, jobs) {
     if (err) console.log(err);
     res.render("viewJob", {
       job: jobs[0]
@@ -35,7 +44,7 @@ router.get("/jobs/:id", function (req, res) {
   });
 });
 
-router.post("/jobs/apply/:jobId", upload.any(), function (req, res) {
+router.post("/jobs/apply/:jobId", upload.any(), function(req, res) {
   let sql =
     "  INSERT INTO Applicants (Name, Email, Phone, CV, JobID) VALUES (?, ?, ?, ?, ?)";
   con.query(
@@ -47,20 +56,20 @@ router.post("/jobs/apply/:jobId", upload.any(), function (req, res) {
       req.files[0].path,
       req.params.jobId
     ],
-    function (err, insertedApplicant) {
+    function(err, insertedApplicant) {
       if (err) console.log(err);
       res.redirect("/jobs");
     }
   );
 });
 
-router.get("/overview", function (req, res) {
+router.get("/overview", function(req, res) {
   res.render("overview");
 });
 
-router.get("/news", function (req, res) {
+router.get("/news", function(req, res) {
   let sql = "SELECT * FROM News";
-  con.query(sql, function (err, news) {
+  con.query(sql, function(err, news) {
     if (err) console.log(err);
     res.render("news", {
       news: news
@@ -68,9 +77,9 @@ router.get("/news", function (req, res) {
   });
 });
 
-router.get("/news/:id", function (req, res) {
+router.get("/news/:id", function(req, res) {
   let sql = "SELECT * FROM News where ID = ?";
-  con.query(sql, [req.params.id], function (err, news) {
+  con.query(sql, [req.params.id], function(err, news) {
     if (err) console.log(err);
     res.render("viewNews", {
       news: news[0]
@@ -78,19 +87,27 @@ router.get("/news/:id", function (req, res) {
   });
 });
 
-router.get("/complaints", function (req, res) {
+router.get("/complaints", function(req, res) {
   res.render("complaints");
 });
 
-router.post("/complaints" , function(req , res){
-    const sql = "INSERT INTO Complaints (StudentFullName, PhoneNumber, email, Department, Grade, Subject, Message) VALUES (?,?,?,?,?,?,?)";
-    const inputData = [req.body.fullName, req.body.phoneNumber, req.body.email, req.body.department, req.body.grade, req.body.typeOfComplaint, req.body.message]
-   
-    con.query(sql , inputData, function(err , data)
-    {
-        if(err) console.log(err);
-        console.log(data);
-        res.render("complaints");
-    });
+router.post("/complaints", function(req, res) {
+  const sql =
+    "INSERT INTO Complaints (StudentFullName, PhoneNumber, email, Department, Grade, Subject, Message) VALUES (?,?,?,?,?,?,?)";
+  const inputData = [
+    req.body.fullName,
+    req.body.phoneNumber,
+    req.body.email,
+    req.body.department,
+    req.body.grade,
+    req.body.typeOfComplaint,
+    req.body.message
+  ];
+
+  con.query(sql, inputData, function(err, data) {
+    if (err) console.log(err);
+    console.log(data);
+    res.render("complaints");
+  });
 });
 module.exports = router;
