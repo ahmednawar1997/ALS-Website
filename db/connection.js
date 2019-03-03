287e9728bcdb9c5e416d0b6b53f98c557f19c6e4
Amr Ramadan <amr_ramadan97@outlook.com>
2019-03-04 01:06:54 +0200

    DB connection password added on use

--- db/connection.js
+++ db/connection.js
@@ -1,18 +1,17 @@
 var mysql = require("mysql");
 
 var con = mysql.createConnection({
-  host: "localhost",
+  host: "167.99.46.128",
   user: "root",
-  password: "password",
+  password: "", // you need to add password to login to DB or use local
   database: "ALS",
   port: 3306
 });
-con.connect(function (err) {
+con.connect(function(err) {
   if (err) {
     console.log("Error connecting to database ! \n" + err);
     return;
-  }
-  else {
+  } else {
     console.log("Database successfully connected");
   }
 });
