a4dbb5618f4c80e015f84f060157fa290426a93b
beltagy97 <43505298+beltagy97@users.noreply.github.com>
2019-03-05 14:33:42 +0200

    changed connection to be connection pool

--- db/connection.js
+++ db/connection.js
@@ -1,19 +1,21 @@
 var mysql = require("mysql");
 
-var con = mysql.createConnection({
+var con = mysql.createPool({
   host: "167.99.46.128",
   user: "root",
   password: "", // you need to add password to login to DB or use local
   database: "ALS",
   port: 3306
 });
-con.connect(function(err) {
-  if (err) {
-    console.log("Error connecting to database ! \n" + err);
-    return;
-  } else {
-    console.log("Database successfully connected");
+
+con.getConnection(function(err,connection){
+  if(err) throw err;
+  else{
+    console.log("Database connected successfullly");
   }
-});
+
+  connection.release();
+
+})
 
 module.exports = con;
